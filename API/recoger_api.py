e7367a47ea9f361cdb8878bed2f2397200a8ae2c
Jaime Conde <jaime.conde.baruque@gmail.com>
2021-10-27 21:06:34 +0200

    Changes:
    - Added 'user-agent' to be able to use the API
    - Modifications to the stores (only the closer ones)
    - Small modification to the Toast notification (not list the stores)

--- API/recoger_api.py
+++ API/recoger_api.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from datetime import datetime
+import json
 from win10toast import ToastNotifier
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -30,29 +31,41 @@ def get_peso_id(peso):
 
 def check_cantidad(content):
     disp = {}
+    s_disp = ''
     for stores in content:
         cantidad = stores['quantity']
         if cantidad != 0:
             disp[stores['storeName']] = cantidad
-    return disp
+            s_disp += stores['storeName'] + ': ' + str(cantidad) + '\n'
+    return disp, s_disp
 
 
 def main(peso, product):
     skuId = get_peso_id(peso)
-    modelId = get_product_id('Discos')
-    storeIds = ['0070189601896', '0070062300623', '0070054700547', '0070008600086', '0070245002450', '0070187701877', '0070010200102', '0070011400114', '0070040100401', '0070140601406', '0070025700257', '0070014200142', '0070050600506', '0070038500385', '0070221102211', '0070032800328', '0070029900299', '0070013700137']
+    modelId = get_product_id(product)
+    '''storeIds = ['0070189601896', '0070062300623', '0070054700547', '0070008600086', '0070245002450', '0070187701877',
+                '0070010200102', '0070011400114', '0070040100401', '0070140601406', '0070025700257', '0070014200142',
+                '0070050600506', '0070038500385', '0070221102211', '0070032800328', '0070029900299', '0070013700137']'''
+    storeIds = ['0070189601896', '0070062300623', '0070245002450', '0070010200102']
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                             ' Chrome/95.0.4638.54 Safari/537.36'}
     wait_time = 1
     # Start the search
     intentos = 1
     toast = ToastNotifier()
     while True:
-        response = requests.get(GLOBVARS['BASE-URL'], params={'storeIds': ','.join(storeIds), 'skuId': skuId, 'modelId': modelId, 'displayStoreDetails': False}, verify=False).json()
-        if response['responseTO']['httpStatus'] == 200:
+        response_raw = requests.get(GLOBVARS['BASE-URL'], headers=headers,
+                                    params={'storeIds': ','.join(storeIds), 'skuId': skuId, 'modelId': modelId,
+                                            'displayStoreDetails': False}, verify=False)
+        if response_raw.status_code == 200:
+            response = response_raw.json()
             content = response['responseTO']['data']
-            disp = check_cantidad(content)
+            disp, s_disp = check_cantidad(content)
             if bool(disp):
-                print(str(disp))
-                toast.show_toast('RÁPIDO', 'Hemos encontrado el producto! mira la consola para más detalle')
+                print(s_disp)
+                toast.show_toast('¡PRODUCTO ENCONTRADO!',
+                                 s_disp,
+                                 threaded=True)
                 break
             else:
                 now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
